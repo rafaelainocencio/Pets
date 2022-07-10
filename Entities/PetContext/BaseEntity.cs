d306a7a923ae6a00a375378727cd6bb8cec02bdb
rafaela montalvao <rafaelainocencioo@gmail.com>
2022-07-10 19:40:09 -0300

    Entidades e entidade abstrata

---
+++ Entities/PetContext/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; private set; } //Global Unique Identifier - Gera um hash onde podemos trabalhar dentro do proprio C#
+    public string FirstName { get; private set; }
+    public string LastName { get; set; }
+    public DateTime DateCreated { get; set; }
+
+
+
+}
---
+++ Entities/VaccineContext/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; private set; }
+    public DateTime CreatedDate { get; private set; }
+
+    protected BaseEntity()
+    {
+        Id = Guid.NewGuid;
+        CreatedDate = DateTime.UtcNow;
+    }
+
+}
